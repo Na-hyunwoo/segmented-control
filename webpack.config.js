fd231fec163fb647842dea5f06186dccd3f364a9
hw <dusdjeks@naver.com>
2022-12-20 23:26:03 +0900

    initial setting

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = () => {
+  const isDevelopment = process.env.NODE_ENV === "productions";
+
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+    devServer: {
+      port: 3000,
+      hot: true,
+    },
+    devtool: isDevelopment ? "eval-source-map" : "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          use: {
+            loader: "ts-loader",
+            options: {
+              configFile: path.resolve(__dirname, "tsconfig.json"),
+            },
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "public/index.html",
+      }),
+    ],
+  };
+};
