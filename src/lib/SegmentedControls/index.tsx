9e43024fcd81396ef85ed94f500840fbd87dba17
hw <dusdjeks@naver.com>
2022-12-29 18:16:22 +0900

    Add tsdoc for dx

--- src/lib/SegmentedControls/index.tsx
+++ src/lib/SegmentedControls/index.tsx
@@ -1,4 +1,4 @@
-import React, { useId, useState } from "react";
+import React, { Dispatch, SetStateAction, useId, useState } from "react";
 import { spring } from "../styles";
 import {
   Wrapper,
@@ -9,6 +9,63 @@ import {
 } from "./styled";
 import { Props } from "./type";
 
+// {
+//   options: {
+//     value: string;
+//     disabled?: boolean;
+//   }[];
+//   setValue: Dispatch<SetStateAction<string>>;
+//   size: "large" | "small";
+//   name: string;
+//   defaultIndex: number;
+// }
+
+/**
+ * 
+ * @param {{ value: string, disabled?: boolean }[]} props.options 세그먼트의 옵션을 value에 담아주세요. 비활성화 하길 원하는 버튼은 disabled 속성을 적용해주세요. 
+ * @param { Dispatch<SetStateAction<string>> } props.setValue 선택된 세그먼트를 string으로 리턴해요
+ * @param { "large" | "small" } props.size 세그먼트의 크기를 정해주세요. large 혹은 small의 값을 가져요.
+ * @param { string } props.name input의 name값을 넣어주세요. 웹 접근성을 위함이에요. 
+ * @param { defaultIndex } props.number 처음 선택되어있길 원하는 값을 넣어주세요. 
+ * @example
+ * ```jsx
+  import React from "react";
+  import { SegmentedControls } from "./lib";
+
+  const App = () => {
+    return (
+      <>
+        <SegmentedControls
+          options={[
+            { value: "one", disabled: true },
+            { value: "two" },
+            { value: "three" },
+          ]}
+          defaultIndex={1}
+          setValue={(val) => console.log(val)}
+          size={"large"}
+          name={"one"}
+        />
+        <div style={{ marginTop: "100px" }}></div>
+        <SegmentedControls
+          options={[
+            { value: "one", disabled: true },
+            { value: "two" },
+            { value: "three" },
+            { value: "four" },
+          ]}
+          defaultIndex={1}
+          setValue={(val) => console.log(val)}
+          size={"small"}
+          name={"two"}
+        />
+      </>
+    );
+  };
+
+  export default App;
+ * ``` 
+ */
 const SegmentedControls = ({
   options,
   setValue,
